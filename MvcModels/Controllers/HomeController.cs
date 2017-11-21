2bddf9c65d91703d0df56e96c270ed4d1cfe7a96
Anthony Leif <tonyleif@hotmail.com>
2017-11-20 21:24:56 -0600

    Page 648

---
+++ MvcModels/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using MvcModels.Models;
+
+namespace MvcModels.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Person[] personData = {new Person { PersonId = 1, FirstName = "Adam", LastName = "Freeman", Role = Role.Admin},
+                                       new Person { PersonId = 2, FirstName = "Jacqui", LastName = "Gryffyth", Role = Role.User},
+                                       new Person { PersonId = 3, FirstName = "John", LastName = "Smith", Role = Role.User},
+                                       new Person { PersonId = 4, FirstName = "Anne", LastName = "Jones", Role = Role.Guest}
+        };
+
+        public ActionResult Index(int id = 1)
+        {
+            Person dataItem = personData.Where(p => p.PersonId == id).First();
+            return View(dataItem);
+        }
+
+        public ActionResult CreatePerson()
+        {
+            return View(new Person());
+        }
+
+        [HttpPost]
+        public ActionResult CreatePerson(Person model)
+        {
+            return View("Index", model);
+        }
+
+        public ActionResult DisplaySummary([Bind(Prefix="HomeAddress", Exclude = "Country")] AddressSummary summary)
+        {
+            return View(summary);
+        }
+
+        public ActionResult Names(string[] names)
+        {
+            names = names ?? new string[0];
+            return View(names);
+        }
+    }
+}
---
+++ MvcModels/Models/AddressSummary.cs
@@ -0,0 +1,11 @@
+﻿using System.Web.Mvc;
+
+namespace MvcModels.Models
+{
+    [Bind(Include = "City")]
+    public class AddressSummary
+    {
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
